ed819499d481635f04a6dcb3afb2a8099780d96d
Nikolay Prizhimin <prinv@yandex.ru>
2024-02-16 22:14:08 +0500

    Аутентификация пользователей (стр. 205)

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == 'POST':
@@ -26,3 +23,10 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
